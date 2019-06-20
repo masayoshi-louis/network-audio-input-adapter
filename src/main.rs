948ef36616ea4ab30527ea7f984f3a95d4b41e12
luyi <luyi@newtranx.com>
2019-06-20 14:38:59 +0800

    solve http header problem
    HQPlayer treat header key as case-sensitive

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+#[macro_use]
+extern crate log;
+
+use std::env;
+
 use futures::future;
 use futures::future::FutureResult;
 use futures::prelude::*;
+use http::header::HdrName;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
 
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info,network_audio_input_adapter=trace");
+    }
+    env_logger::init();
+
     let addr = ([0, 0, 0, 0], 3000).into();
 
     let server = Server::bind(&addr)
         .serve(|| service_fn(handle))
         .map_err(|e| eprintln!("server error: {}", e));
 
-    println!("Listening on http://{}", addr);
+    info!("Listening on http://{}", addr);
     hyper::rt::run(server);
 }
 
@@ -20,11 +31,11 @@ fn handle(req: Request<Body>) -> FutureResult<Response<Body>, hyper::Error> {
 
     match (req.method(), req.uri().path()) {
         (&Method::HEAD, "/stream.raw") => {
-            println!("get head request");
+            trace!("HEAD request received");
             set_headers(&mut response);
         }
         (&Method::GET, "/stream.raw") => {
-            println!("get stream request");
+            trace!("GET request received");
             set_headers(&mut response);
             let (tx, body) = Body::channel();
         }
@@ -40,9 +51,9 @@ fn handle(req: Request<Body>) -> FutureResult<Response<Body>, hyper::Error> {
 }
 
 fn set_headers(response: &mut Response<Body>) {
-    response.headers_mut().insert("Content-Type", "application/x-hqplayer-raw".parse().unwrap());
-    response.headers_mut().insert("X-HQPlayer-Raw-Title", "Roon".parse().unwrap());
-    response.headers_mut().insert("X-HQPlayer-Raw-SampleRate", "44100".parse().unwrap());
-    response.headers_mut().insert("X-HQPlayer-Raw-Channels", "2".parse().unwrap());
-    response.headers_mut().insert("X-HQPlayer-Raw-Format", "int16le".parse().unwrap());
+    response.headers_mut().insert2(HdrName::custom(b"Content-Type", true), "application/x-hqplayer-raw".parse().unwrap());
+    response.headers_mut().insert2(HdrName::custom(b"X-HQPlayer-Raw-Title", true), "NetworkInput".parse().unwrap());
+    response.headers_mut().insert2(HdrName::custom(b"X-HQPlayer-Raw-SampleRate", true), "44100".parse().unwrap());
+    response.headers_mut().insert2(HdrName::custom(b"X-HQPlayer-Raw-Channels", true), "2".parse().unwrap());
+    response.headers_mut().insert2(HdrName::custom(b"X-HQPlayer-Raw-Format", true), "int16le".parse().unwrap());
 }
